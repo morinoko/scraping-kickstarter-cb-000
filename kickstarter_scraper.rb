7b889c3be28b2a6b7fee4fea8f63ab9e506952a4
Felice Forby <feliceforby@gmail.com>
2018-05-17 23:08:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,36 +2,8 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-
-  # Use File.read instead of open-uri because it's not a live site
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  kickstarter_projects = kickstarter.css("li.project.grid_4")
-
-  projects = {}
-  # projects: kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("ul.project-meta span.location-name").text
-  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-
-  kickstarter_projects.each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-    }
-  end
-
-  projects
-
-end
-
+# Create a data hash formatted as below:
+#
 # :projects => {
 #   "My Great Project"  => {
 #     :image_link => "Image Link",
@@ -46,3 +18,27 @@ end
 #     :percent_funded => "Percent Funded"
 #   }
 # }
+
+def create_project_hash
+
+  # Use File.read instead of open-uri because it's not a live site
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  kickstarter_projects = kickstarter.css("li.project.grid_4")
+
+  projects = {}
+
+  kickstarter_projects.each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
+  end
+
+  projects
+
+end
