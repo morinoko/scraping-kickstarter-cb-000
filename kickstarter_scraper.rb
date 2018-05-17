9e5708848f265fd5ef7bbc871f5ad0606eb584e5
Felice Forby <feliceforby@gmail.com>
2018-05-17 23:02:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,19 +23,21 @@ def create_project_hash
     projects[title.to_sym] = {}
   end
 
+  projects
+
 end
 
-:projects => {
-  "My Great Project"  => {
-    :image_link => "Image Link",
-    :description => "Description",
-    :location => "Location",
-    :percent_funded => "Percent Funded"
-  },
-  "Another Great Project" => {
-    :image_link => "Image Link",
-    :description => "Description",
-    :location => "Location",
-    :percent_funded => "Percent Funded"
-  }
-}
+# :projects => {
+#   "My Great Project"  => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   },
+#   "Another Great Project" => {
+#     :image_link => "Image Link",
+#     :description => "Description",
+#     :location => "Location",
+#     :percent_funded => "Percent Funded"
+#   }
+# }
