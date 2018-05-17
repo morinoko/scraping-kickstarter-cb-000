b78c760a1e7b28fcde30586c1618122168c39aca
Felice Forby <feliceforby@gmail.com>
2018-05-17 22:52:12 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,10 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta li a")
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
   project = kickstarter.css("li.project.grid_4").first
   title = project.css("h2.bbcard_name strong a")
   binding.pry
