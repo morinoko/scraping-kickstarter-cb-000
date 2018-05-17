0597c2430fa8ac0ec91eacf3ef14ad8a34612483
Felice Forby <feliceforby@gmail.com>
2018-05-17 22:24:29 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,5 +2,11 @@
 require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  
+  # Use File.read instead of open-uri because it's not a live site
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  projects = kickstarter.css("li.project.grid_4").first
+  
 end
