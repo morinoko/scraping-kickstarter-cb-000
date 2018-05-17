ffa9aa5c3f9208fe210501aa403b3a371d07bc32
Felice Forby <feliceforby@gmail.com>
2018-05-17 23:01:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   # Use File.read instead of open-uri because it's not a live site
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  kickstarter_projects = kickstarter.css("li.project.grid_4")
 
   projects = {}
   # projects: kickstarter.css("li.project.grid_4")
@@ -16,8 +17,24 @@ def create_project_hash
   # location: project.css("ul.project-meta span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
-  project = kickstarter.css("li.project.grid_4").first
-  title = project.css("h2.bbcard_name strong a")
-  binding.pry
+  kickstarter_projects.each do |project|
+    
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
 
 end
+
+:projects => {
+  "My Great Project"  => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  },
+  "Another Great Project" => {
+    :image_link => "Image Link",
+    :description => "Description",
+    :location => "Location",
+    :percent_funded => "Percent Funded"
+  }
+}
